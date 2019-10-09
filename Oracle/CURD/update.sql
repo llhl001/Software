a49c380b346f8ab32f5098b27be10ac6e6d7a893
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2019-10-09 10:35:04 +0800

    Update update.sql

--- Oracle/CURD/update.sql
+++ Oracle/CURD/update.sql
@@ -1,1 +1,13 @@
-d
+
+/********************************* Update ******************************************/
+update tb
+set col1=val1, col2=val2, col3=val3
+where ;
+
+
+update student
+set (sname, sage, sbirthday, saddress)=(
+    select '李四', 20, to_date('2010-01-01', 'yyyy-mm-dd'), '广州市越秀区' 
+    from dual
+)
+where ;
