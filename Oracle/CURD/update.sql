617bd669b64b480ab927628fba7dc0080464d86b
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2019-10-17 10:41:03 +0800

    Update update.sql

--- Oracle/CURD/update.sql
+++ Oracle/CURD/update.sql
@@ -7,7 +7,6 @@ where ;
 
 update student
 set (sname, sage, sbirthday, saddress)=(
-    select '李四', 20, to_date('2010-01-01', 'yyyy-mm-dd'), '广州市越秀区' 
-    from dual
+    select '李四', 20, to_date('2010-01-01', 'yyyy-mm-dd'), '广州市越秀区' from dual  -- 注意此处只能返回单行值
 )
 where ;
