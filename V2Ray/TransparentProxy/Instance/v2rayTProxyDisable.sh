4f4686908191fdef7da24081d163643c24e8b1a6
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2019-11-13 01:13:21 +0800

    Delete v2rayTProxyDisable.sh

--- V2Ray/TransparentProxy/Instance/v2rayTProxyDisable.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# todo 撤销上面的操作
-iptables -t nat -D OUTPUT -p tcp -j V2RAY_LOCAL 
-iptables -t nat -D OUTPUT -p udp -j V2RAY_LOCAL 
-iptables -t nat -F V2RAY_LOCAL
-iptables -t nat -X V2RAY_LOCAL
